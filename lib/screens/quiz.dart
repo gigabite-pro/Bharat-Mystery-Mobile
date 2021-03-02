9cf7dac49cb00704fefe45729b06b58068dab420
gamer-12748 <aayushgarg2006@gmail.com>
2021-03-02 15:57:33 +0530

    dark mode full app quiz also

--- lib/screens/quiz.dart
+++ lib/screens/quiz.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -29,7 +28,7 @@ class _QuizState extends State<Quiz> {
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return Scaffold(
-            backgroundColor: Color(0xffA0E7E5),
+            backgroundColor: Theme.of(context).accentColor,
             body: Center(
               child: CircularProgressIndicator(),
             ),
@@ -38,96 +37,107 @@ class _QuizState extends State<Quiz> {
 
         final allQues = json.decode(snapshot.data);
 
-        return Scaffold(
-          backgroundColor: Color(0xffA0E7E5),
-          body: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(10.0, 60.0, 10.0, 10.0),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Quiz Time",
-                          style: TextStyle(
-                              fontFamily: 'LexendDeca',
-                              fontSize: 27.0,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.black),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    Form(
-                      child: Column(
-                        children: <Widget>[
-                          ListView.builder(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            itemCount: allQues.length,
-                            itemBuilder: (context, index) {
-                              return Column(
-                                children: <Widget>[
-                                  Text(
-                                    (allQues[index])["ques"],
-                                    style: TextStyle(
-                                        fontFamily: 'LexendDeca',
-                                        fontSize: 20.0,
-                                        fontWeight: FontWeight.w200,
-                                        color: Colors.black),
-                                  ),
-                                  SizedBox(
-                                    height: 10.0,
-                                  ),
-                                  TextFormField(
-                                    decoration: InputDecoration(
-                                        hintText: "Answer",
-                                        filled: true,
-                                        fillColor: Colors.white,
-                                        hintStyle:
-                                            TextStyle(color: Colors.black),
-                                        border: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Colors.black, width: 1.5),
-                                          borderRadius:
-                                              BorderRadius.circular(10.0),
-                                        ),
-                                        contentPadding: EdgeInsets.all(17.0)),
-                                  ),
-                                  SizedBox(
-                                    height: 20.0,
-                                  ),
-                                ],
-                              );
-                            },
-                          ),
-                          SizedBox(
-                            height: 5.0,
-                          ),
-                          MaterialButton(
-                            onPressed: () {},
-                            height: 50.0,
-                            padding: EdgeInsets.symmetric(horizontal: 40.0),
-                            shape: StadiumBorder(),
-                            child: Text(
-                              "Submit",
-                              style: TextStyle(
-                                  fontFamily: 'LexendDeca',
-                                  fontSize: 16.0,
-                                  color: Colors.white),
-                            ),
-                            color: Colors.black,
-                          ),
-                        ],
+        return MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).accentColor,
+              title: Text(
+                "Quiz Time",
+                style: TextStyle(
+                    fontFamily: 'LexendDeca',
+                    fontSize: 27.0,
+                    fontWeight: FontWeight.w700,
+                    color: Theme.of(context).highlightColor),
+              ),
+              leading: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Theme.of(context).highlightColor,
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ),
+            backgroundColor: Theme.of(context).accentColor,
+            body: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 60.0, 10.0, 10.0),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 30.0,
                       ),
-                    )
-                  ],
+                      //form
+                      Form(
+                        child: Column(
+                          children: <Widget>[
+                            ListView.builder(
+                              scrollDirection: Axis.vertical,
+                              shrinkWrap: true,
+                              itemCount: allQues.length,
+                              itemBuilder: (context, index) {
+                                return Column(
+                                  children: <Widget>[
+                                    //the question
+                                    Text(
+                                      (allQues[index])["ques"],
+                                      style: TextStyle(
+                                          fontFamily: 'LexendDeca',
+                                          fontSize: 20.0,
+                                          color: Colors.white),
+                                    ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    //answer text field
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                          hintText: "Answer",
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                          hintStyle:
+                                              TextStyle(color: Colors.black),
+                                          border: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.black,
+                                                width: 1.5),
+                                            borderRadius:
+                                                BorderRadius.circular(10.0),
+                                          ),
+                                          contentPadding: EdgeInsets.all(17.0)),
+                                    ),
+                                    SizedBox(
+                                      height: 20.0,
+                                    ),
+                                  ],
+                                );
+                              },
+                            ),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            //submit button
+                            MaterialButton(
+                              onPressed: () {},
+                              height: 50.0,
+                              padding: EdgeInsets.symmetric(horizontal: 40.0),
+                              shape: StadiumBorder(),
+                              child: Text(
+                                "Submit",
+                                style: TextStyle(
+                                    fontFamily: 'LexendDeca',
+                                    fontSize: 16.0,
+                                    color: Theme.of(context).cardColor),
+                              ),
+                              color: Theme.of(context).highlightColor,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
